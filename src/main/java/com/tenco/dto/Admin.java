4fc1d57e4a1659141e4574505afb4510aaa4cc94
Your Name <fosem@gmail.com>
2026-04-09 10:45:42 +0900

    DTO설계

--- src/main/java/com/tenco/dto/Admin.java
+++ src/main/java/com/tenco/dto/Admin.java
@@ -1,4 +1,15 @@
 package com.tenco.dto;
 
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Admin {
+    Integer id;
+    String admin_id;
+    String password;
+    String name;
 }
--- src/main/java/com/tenco/dto/Sales.java
+++ src/main/java/com/tenco/dto/Sales.java
@@ -1,4 +1,17 @@
 package com.tenco.dto;
 
+import lombok.*;
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Sales {
+    private String id;
+    private Integer product_id;
+    private Integer quantity;
+    private Double unit_price;
+    private LocalDate sold_at;
 }
